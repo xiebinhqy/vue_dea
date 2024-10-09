d2718ec573997500225d1a1497aa70d778af46fa
xiebinhqy <xiebin19900104@gmail.com>
2024-10-09 19:00:32 +0800

    model zanshi

---
+++ my-docs/src/.vuepress/config.ts
@@ -0,0 +1,18 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "文档演示",
+  description: "vuepress-theme-hope 的文档演示",
+  // zhege shi shenm 
+  
+
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
