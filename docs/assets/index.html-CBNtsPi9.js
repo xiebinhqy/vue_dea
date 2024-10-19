2a1f49f3d9e6ddc9e4bcaacc498dc1a43b50ba43
xiebinhqy <xiebin19900104@gmail.com>
2024-10-19 10:59:43 +0800

    base-gg-build

---
+++ docs/assets/index.html-CBNtsPi9.js
@@ -0,0 +1,11 @@
+import{_ as n}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as o,e as a,a as e,o as i}from"./app-DbmJbDcN.js";const r={};function l(p,t){return i(),o("div",null,[a(` ---
+title: 外汇券商选择
+index: true
+icon: laptop-code
+category:
+  - 测试指南
+  - 马丁
+  - 剥头皮测试
+---
+
+// <Catalog /> `),t[0]||(t[0]=e("figure",null,[e("img",{src:"https://img.deattor.us.kg/file/1728391105089_lhtz.png",alt:"alt text",tabindex:"0",loading:"lazy"}),e("figcaption",null,"alt text")],-1)),t[1]||(t[1]=e("p",null,"这个是目录，也可以写一点其他东西",-1)),t[2]||(t[2]=e("p",null,"这个是增加了indexwenjian 的效果",-1))])}const c=n(r,[["render",l],["__file","index.html.vue"]]),g=JSON.parse('{"path":"/Forex/ebc/","title":"","lang":"zh-CN","frontmatter":{"description":"alt textalt text 这个是目录，也可以写一点其他东西 这个是增加了indexwenjian 的效果","gitInclude":[],"head":[["meta",{"property":"og:url","content":"https://vuepress-theme-hope-docs-demo.netlify.app/Forex/ebc/"}],["meta",{"property":"og:site_name","content":"文档演示"}],["meta",{"property":"og:description","content":"alt textalt text 这个是目录，也可以写一点其他东西 这个是增加了indexwenjian 的效果"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:image","content":"https://img.deattor.us.kg/file/1728391105089_lhtz.png"}],["meta",{"property":"og:locale","content":"zh-CN"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"\\",\\"image\\":[\\"https://img.deattor.us.kg/file/1728391105089_lhtz.png\\"],\\"dateModified\\":null,\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"dea.bin\\",\\"url\\":\\"https://mister-hope.com\\"}]}"]]},"headers":[],"readingTime":{"minutes":0.19,"words":56},"filePathRelative":"Forex/ebc/README.md","autoDesc":true,"excerpt":"<!-- ---\\ntitle: 外汇券商选择\\nindex: true\\nicon: laptop-code\\ncategory:\\n  - 测试指南\\n  - 马丁\\n  - 剥头皮测试\\n---\\n\\n// <Catalog /> -->\\n<figure><img src=\\"https://img.deattor.us.kg/file/1728391105089_lhtz.png\\" alt=\\"alt text\\" tabindex=\\"0\\" loading=\\"lazy\\"><figcaption>alt text</figcaption></figure>\\n<p>这个是目录，也可以写一点其他东西</p>\\n<p>这个是增加了indexwenjian 的效果</p>"}');export{c as comp,g as data};
